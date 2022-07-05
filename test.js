fbcfe0a1d3c64bb3597c43e5d4fc3c45d78c79d8
DavidFirmansyahSiregar <108645867+DavidFirmansyahSiregar@users.noreply.github.com>
2022-07-05 16:05:28 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+// let x = 5;
+// console.log(x+x);
+
+// let hope = "sad";
+// console.log(hope)
+// hope = "happy"
+// console.log(hope)
+
+// var time1 = "pagi";
+// const activity = "makan";
+// console.log(activity + time1);
+
+// let number = 11;
+// while(number<=12){console.log(number)};
+// number = number + 1;
+
+console.log(-2 < -1);
+console.log(2 > 1);
